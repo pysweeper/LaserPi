cca0dbb327eaa51d640e902c900cc0f810ab86aa
BenjaminWyss <b040w377@ku.edu>
2019-04-01 09:37:19 -0500

    Add functionality for gun ids.

--- python/guns.py
+++ python/guns.py
@@ -1,8 +1,26 @@
 import connect
 
+id = 0
+
+#Read the gun id from the text file 'gunid''
+try:
+  file = open('gunid', 'rb+')
+  file.seek(-2, 2)
+  id = int(file.read(1))
+  file.close()
+except Exception:
+  print("Gun not registered. Please open gunid.dist and follow the written instructions.")
+  quit()
+
+#Gunid 0 is reserved for the database's NULL gun
+if (id == 0):
+  print("Gun id cannot be 0. Please open gunid.dist and follow the written instructions.")
+  quit()
+
+
 def fireShot():
   mydb = connect.connect()
-  sql = "UPDATE Guns SET shots_fired = shots_fired + 1 WHERE gun = 0"
+  sql = "UPDATE Guns SET shots_fired = shots_fired + 1 WHERE gun = " + str(id)
   mycursor = mydb.cursor()
   mycursor.execute(sql)
   mydb.commit()
