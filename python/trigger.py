a630f01a0b5d43c3a47d5bfacabf50fad4347e12
Jon Volden <volden.jon@gmail.com>
2019-04-03 21:07:39 +0100

    Updated gunid for trigger.

--- python/trigger.py
+++ python/trigger.py
@@ -1,28 +1,36 @@
 import os, datetime, time
 import RPi.GPIO as GPIO
 from subprocess import call
+from gun import Gun
 
-GPIO.setmode(GPIO.BCM)
-TRIGGER=2
-GPIO.setup(TRIGGER, GPIO.IN)
 
-def shoot(pin):
+class Trigger:
+
+  def __init__(self):
+    self.TRIGGER=2
+    self.gun = Gun()
+    self.gun.readIDFile()
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(self.TRIGGER, GPIO.IN)
+  
+  def shoot(self, pin):
     if GPIO.input(pin) == GPIO.LOW:
-        print(str(datetime.datetime.now()), "Shot")
-        call(["irsend","SEND_ONCE","laserpi","Shot02"])
-
-def addTrigger():
-    GPIO.add_event_detect(TRIGGER, GPIO.FALLING, callback=shoot, bouncetime=50)
-
-def deleteTrigger():
-    GPIO.remove_event_detect(TRIGGER)
+      print(str(datetime.datetime.now()), "Shot")
+      self.gun.fireShot()
+      call(["irsend","SEND_ONCE","laserpi","Shot02"])
+  
+  def addTrigger(self):
+    GPIO.add_event_detect(self.TRIGGER, GPIO.FALLING, callback=self.shoot, bouncetime=50)
+  
+  def deleteTrigger(self):
+    GPIO.remove_event_detect(self.TRIGGER)
     GPIO.cleanup()
-
+  
 if __name__ == "__main__":
-    try:
-        addTrigger()
-        while True:
-            pass
-    finally:
-        deleteTrigger()
-
+  try:
+    trigger = Trigger()
+    trigger.addTrigger()
+    while True:
+      pass
+  finally:
+    trigger.deleteTrigger()
