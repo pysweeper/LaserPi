431d13fecbb1ffac7226d2fc07c3ee354bafaff2
Jon Volden <volden.jon@gmail.com>
2019-04-19 11:44:24 -0500

    Updated python files. Cleaned up excess and added first tests.

--- gunpi-python/gun.py
+++ gunpi-python/gun.py
@@ -13,6 +13,8 @@ class Gun:
     """
     self.id = 0
     self.username = "None"
+    self.mydb = connect.connect()
+    self.cursor = self.mydb.cursor()
 
   def readIDFile(self):
     """ readIDFile
@@ -35,7 +37,6 @@ class Gun:
     except Exception:
       print("gunid file not found. Please open gunid.dist and follow the written instructions.")
       quit()
-
     self.validate()
 
   def validate(self):
@@ -49,21 +50,20 @@ class Gun:
         Postconditions: The gunid and username will be set within the 
         database for record keeping.
     """
-    #Gunid 0 is reserved for the database's NULL gun
     if (self.id == 0):
       print("Gun id cannot be 0. Please open gunid.dist and follow the written instructions.")
       quit()
-    mydb = connect.connect()
-    sql = "SELECT * FROM Guns WHERE gun=" + str(self.id)
-    mycursor = mydb.cursor()
-    mycursor.execute(sql)
-    myresult = mycursor.fetchall()
+    sql = ("SELECT * FROM Guns "
+           "WHERE gun={}").format(self.id)
+    self.cursor.execute(sql)
+    myresult = self.cursor.fetchall()
     if (len(myresult) == 0):
       print("Gun id not registered. Please go to https://people.eecs.ku.edu/~b040w377/laserpi.html to register a new gun.")
       quit()
-    sql = "SELECT * FROM Players WHERE username='" + self.username + "'"
-    mycursor.execute(sql)
-    myresult = mycursor.fetchall()
+    sql = ("SELECT * FROM Players "
+           "WHERE username='{}'").format(self.username)
+    self.cursor.execute(sql)
+    myresult = self.cursor.fetchall()
     if (len(myresult) == 0):
       print("Username not registered. Please go to https://people.eecs.ku.edu/~b040w377/laserpi.html to register a new player.")
       quit()
@@ -78,16 +78,15 @@ class Gun:
         Postconditions: The database will be updated to reflect that 
         the user of the gun fired a shot.
     """
-    mydb = connect.connect()
-    sql = "UPDATE Guns SET shots_fired = shots_fired + 1 WHERE gun = " + str(self.id)
-    mycursor = mydb.cursor()
-    mycursor.execute(sql)
-    mydb.commit()
-    print(str(datetime.datetime.now()), "Gun shots updated: ", mycursor.rowcount, "record(s) affected")
+    sql = ("UPDATE Guns SET shots_fired = shots_fired + 1 "
+           "WHERE gun = '{}'").format(self.id)
+    self.cursor.execute(sql)
+    self.mydb.commit()
+    print(str(datetime.datetime.now()), "Gun shots updated: ", self.cursor.rowcount, "record(s) affected")
     sql = "UPDATE Players SET shots_fired = shots_fired + 1 WHERE username='" + self.username + "'"
-    mycursor.execute(sql)
-    mydb.commit()
-    print(str(datetime.datetime.now()), "Player shot updated: ", mycursor.rowcount, "record(s) affected")
+    self.cursor.execute(sql)
+    self.mydb.commit()
+    print(str(datetime.datetime.now()), "Player shot updated: ", self.cursor.rowcount, "record(s) affected")
 
   def joinGame(self):
     """ joinGame
@@ -98,20 +97,19 @@ class Gun:
         Postconditions: A game will be joined or a message will
         be displayed that there is no game to join.
     """
-    mydb = connect.connect()
     sql = "SELECT * FROM Games WHERE current_state = 1"
-    mycursor = mydb.cursor()
-    mycursor.execute(sql)
-    myresult = mycursor.fetchall()
+    self.cursor.execute(sql)
+    myresult = self.cursor.fetchall()
     if (len(myresult) == 0):
-      print(str(datetime.datetime.now()), "Could not find a game to join")
+      print(("{}: Could not find a game to join").format(datetime.datetime.now()))
       return False
     elif (len(myresult) == 1):
       gameid = myresult[0][0]
-      sql = "INSERT INTO Game_Users (game_id, gun_id, username) VALUES ("+str(gameid)+", "+str(self.id)+", '"+self.username+"')"
-      mycursor.execute(sql)
-      mydb.commit()
-      print(str(datetime.datetime.now()), "Inserted gun data: ", mycursor.rowcount, "record(s) affected")
+      sql = ("INSERT INTO Game_Users (game_id, gun_id, username) "
+             "VALUES ({}, {}, {})").format(gameid, self.id, self.username)
+      self.cursor.execute(sql)
+      self.mydb.commit()
+      print(("{}: Inserted gun data: {} record(s) affected").format(datetime.datetime.now(), self.cursor.rowcount))
       return True
 
   def checkGame(self):
@@ -122,11 +120,10 @@ class Gun:
         Postconditions: If there is no active game a message will be 
         displayed stating such.
     """
-    mydb = connect.connect()
-    sql = "SELECT * FROM Games WHERE current_state in (1,2)"
-    mycursor = mydb.cursor()
-    mycursor.execute(sql)
-    myresult = mycursor.fetchall()
+    sql = ("SELECT * FROM Games "
+           "WHERE current_state in (1,2)")
+    self.cursor.execute(sql)
+    myresult = self.cursor.fetchall()
     if (len(myresult) == 0):
       print(str(datetime.datetime.now()), "Could not find a game to join")
       return False
@@ -143,27 +140,30 @@ class Gun:
         Postconditions: The database is updated with the proper stats 
         and the game is ended.
     """
-    mydb = connect.connect()
-    sql = "SELECT * FROM (Games INNER JOIN Game_Users ON Games.id = Game_Users.game_id) WHERE Games.current_state = 2 AND Game_Users.gun_id <> " + str(self.id)
-    mycursor = mydb.cursor()
-    mycursor.execute(sql)
-    myresult = mycursor.fetchall()
+    sql = ("SELECT * FROM (Games INNER JOIN Game_Users ON Games.id = Game_Users.game_id) "
+           "WHERE Games.current_state = 2 AND Game_Users.gun_id <> {}").format(self.id)
+    self.cursor.execute(sql)
+    myresult = self.cursor.fetchall()
     if (len(myresult) == 0):
       print(str(datetime.datetime.now()), "Could not find an active game.")
     else:
       opponentGun = myresult[0][5]
       opponentName = myresult[0][6]
-      sql = "UPDATE Players SET losses = losses + 1 WHERE username='" + self.username + "'"
-      mycursor.execute(sql)
-      sql = "UPDATE Players SET wins = wins + 1 WHERE username='" + opponentName + "'"
-      mycursor.execute(sql)
-      sql = "UPDATE Guns SET losses = losses + 1 WHERE gun='" + str(self.id) + "'"
-      mycursor.execute(sql)
+      sql = ("UPDATE Players SET losses = losses + 1 "
+             "WHERE username='{}'").format(self.username)
+      self.cursor.execute(sql)
+      sql = ("UPDATE Players SET wins = wins + 1 "
+             "WHERE username='{}'").format(opponentName)
+      self.cursor.execute(sql)
+      sql = ("UPDATE Guns SET losses = losses + 1 "
+             "WHERE gun='{}'").format(self.id)
+      self.cursor.execute(sql)
       sql = "UPDATE Guns SET wins = wins + 1 WHERE gun='" + str(opponentGun) + "'"
-      mycursor.execute(sql)
-      sql = "UPDATE Games SET winner = " + str(opponentGun) + ", current_state = 0 WHERE current_state = 2"
-      mycursor.execute(sql)
-      mydb.commit()
+      self.cursor.execute(sql)
+      sql = ("UPDATE Games SET winner = '{}', current_state = 0 "
+             "WHERE current_state = 2").format(opponentGun)
+      self.cursor.execute(sql)
+      self.mydb.commit()
 
 
   def dumpGuns(self):
@@ -173,15 +173,10 @@ class Gun:
         Postcondition: The gunid will be printed to the 
         console.
     """
-    mydb = connect.connect()
-    mycursor = mydb.cursor()
-    mycursor.execute("SELECT * FROM Guns")
-    myresult = mycursor.fetchall()
-    for x in myresult:
+    self.cursor.execute("SELECT * FROM Guns")
+    for x in self.cursor.fetchall():
       print(x)
 
 if __name__ == "__main__":
-  x = Gun()
-  x.readIDFile()
-  x.joinGame()
-  x.loseGame()
+  print("Cannot be ran directly. Run laserpi.py to start the gun program. \n"
+        "Or run test/test.py to test modules.")
