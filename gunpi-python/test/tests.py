ce65bba125722b981be2b229dff0224e654dd4fb
BenjaminWyss <b040w377@ku.edu>
2019-04-25 12:40:24 -0500

    Update gitignore for unit testing gunid file

--- gunpi-python/test/tests.py
+++ gunpi-python/test/tests.py
@@ -2,14 +2,14 @@ import unittest
 import sys
 sys.path.append('../')
 import connect
-import gun
+from gun import Gun
 import mysql.connector
 
 class TestLaserPi(unittest.TestCase):
     def setUp(self):
         pass
 
-    def test_connect(self):
+    def test_connectReturnsInstanceOfMySQLConnection(self):
         '''
         Test for connect.py
         Should return a mysqld object.
@@ -23,6 +23,9 @@ class TestLaserPi(unittest.TestCase):
         Test for gun.validate()
         Should ...
         '''
+        gun = Gun()
+        gun.readIDFile()
+        print(gun.id)
         pass
 
 if __name__ == '__main__':
