6f8e43c3eae630b18366849bea1dc7bb7a57f2c3
benjaminwyss <wyssbenjamin17@gmail.com>
2019-04-26 16:13:41 -0500

    Add more tests involving fetching database values.

--- gunpi-python/test/tests.py
+++ gunpi-python/test/tests.py
@@ -149,6 +149,35 @@ class TestLaserPi(unittest.TestCase):
 
       mydb.close()
 
+    def test_joinGameCorrectlyAddsUserIntoGame(self):
+      file = open('gunid', 'r+')
+      file.truncate()
+      file.write("# User Identification File.\n# Change gunid and username.\n# Rename file to gunid.\ngunid=3\nusername=Benjamin\n")
+      file.close()
+      gun = Gun()
+      gun.readIDFile()
+
+      mydb = connect.connect()
+      cursor = mydb.cursor()
+      sql = "INSERT INTO Games (current_state, winner, game_date) VALUES (1, 0, (NOW() - INTERVAL 4 HOUR + INTERVAL 11 MINUTE - INTERVAL 22 SECOND))"
+      cursor.execute(sql)
+      mydb.commit()
+
+      gun.joinGame()
+
+      sql = "SELECT * FROM (Games INNER JOIN Game_Users ON Games.id = Game_Users.game_id) WHERE Games.current_state = 1"
+      cursor.execute(sql)
+      myresult = cursor.fetchall()
+      self.assertEqual(len(myresult), 1) #only one row added to database
+      self.assertEqual(myresult[0][5], 3) #gunid is 3
+      self.assertEqual(myresult[0][6], "Benjamin") #username is Benjamin
+
+      sql = "UPDATE Games SET current_state=0 WHERE current_state=1"
+      cursor.execute(sql)
+      mydb.commit()
+
+      mydb.close()
+
     def test_checkGameReturnsFalseWhenThereIsNoActiveGame(self):
       file = open('gunid', 'r+')
       file.truncate()
@@ -264,6 +293,105 @@ class TestLaserPi(unittest.TestCase):
 
       mydb.close()
 
+    def test_loseGameCorrectlyUpdatesLoserStats(self):
+      file = open('gunid', 'r+')
+      file.truncate()
+      file.write("# User Identification File.\n# Change gunid and username.\n# Rename file to gunid.\ngunid=3\nusername=Benjamin\n")
+      file.close()
+      gun = Gun()
+      gun.readIDFile()
+
+      file = open('gunid', 'r+')
+      file.truncate()
+      file.write("# User Identification File.\n# Change gunid and username.\n# Rename file to gunid.\ngunid=4\nusername=Thomas\n")
+      file.close()
+      gun2 = Gun()
+      gun2.readIDFile()
+
+      mydb = connect.connect()
+      cursor = mydb.cursor()
+      sql = "INSERT INTO Games (current_state, winner, game_date) VALUES (2, 0, (NOW() - INTERVAL 4 HOUR + INTERVAL 11 MINUTE - INTERVAL 22 SECOND))"
+      cursor.execute(sql)
+      mydb.commit()
+
+      sql = "SELECT * FROM Guns WHERE gun=4"
+      cursor.execute(sql)
+      myresult = cursor.fetchall()
+      oldGunLosses = myresult[0][2]
+
+      sql = "SELECT * FROM Players WHERE username='Thomas'"
+      cursor.execute(sql)
+      myresult = cursor.fetchall()
+      oldPlayerLosses = myresult[0][2]
+
+      gun.joinGame()
+      gun2.joinGame()
+      gun2.loseGame()
+
+      sql = "SELECT * FROM Guns WHERE gun=4"
+      cursor.execute(sql)
+      myresult = cursor.fetchall()
+      newGunLosses = myresult[0][2]
+
+      sql = "SELECT * FROM Players WHERE username='Thomas'"
+      cursor.execute(sql)
+      myresult = cursor.fetchall()
+      newPlayerLosses = myresult[0][2]
+
+      self.assertEqual((oldGunLosses + 1), newGunLosses)
+      self.assertEqual((oldPlayerLosses + 1), newPlayerLosses)
+
+      mydb.close()
+
+    def test_loseGameCorrectlyUpdatesWinnerStats(self):
+      file = open('gunid', 'r+')
+      file.truncate()
+      file.write("# User Identification File.\n# Change gunid and username.\n# Rename file to gunid.\ngunid=3\nusername=Benjamin\n")
+      file.close()
+      gun = Gun()
+      gun.readIDFile()
+
+      file = open('gunid', 'r+')
+      file.truncate()
+      file.write("# User Identification File.\n# Change gunid and username.\n# Rename file to gunid.\ngunid=4\nusername=Thomas\n")
+      file.close()
+      gun2 = Gun()
+      gun2.readIDFile()
+
+      mydb = connect.connect()
+      cursor = mydb.cursor()
+      sql = "INSERT INTO Games (current_state, winner, game_date) VALUES (2, 0, (NOW() - INTERVAL 4 HOUR + INTERVAL 11 MINUTE - INTERVAL 22 SECOND))"
+      cursor.execute(sql)
+      mydb.commit()
+
+      sql = "SELECT * FROM Guns WHERE gun=3"
+      cursor.execute(sql)
+      myresult = cursor.fetchall()
+      oldGunWins = myresult[0][1]
+
+      sql = "SELECT * FROM Players WHERE username='Benjamin'"
+      cursor.execute(sql)
+      myresult = cursor.fetchall()
+      oldPlayerWins = myresult[0][1]
+
+      gun.joinGame()
+      gun2.joinGame()
+      gun2.loseGame()
+
+      sql = "SELECT * FROM Guns WHERE gun=3"
+      cursor.execute(sql)
+      myresult = cursor.fetchall()
+      newGunWins = myresult[0][1]
+
+      sql = "SELECT * FROM Players WHERE username='Benjamin'"
+      cursor.execute(sql)
+      myresult = cursor.fetchall()
+      newPlayerWins = myresult[0][1]
+
+      self.assertEqual((oldGunWins + 1), newGunWins)
+      self.assertEqual((oldPlayerWins + 1), newPlayerWins)
+
+      mydb.close()
 
 if __name__ == '__main__':
     unittest.main()
