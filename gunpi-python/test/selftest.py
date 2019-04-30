4d5c0b573695f0b6518a1198ce2a00d4d8b112a2
Jon Volden <volden.jon@gmail.com>
2019-04-30 12:05:36 -0500

    Added selftest.py template.

---
+++ gunpi-python/test/selftest.py
@@ -0,0 +1,23 @@
+import unittest, sys
+sys.path.append('../')
+from connect import connect
+from gun import Gun
+from led import LED
+
+def main(self):
+    '''
+        Test LED
+            Blink Red
+            Solid Red
+            Blink Green
+            Solid Green
+        Test Trigger
+            Push trigger
+            Hold trigger
+        Test Send fake IR code
+            Listen for callback
+    '''
+    pass
+
+if __name__ == "__main__":
+    main()
